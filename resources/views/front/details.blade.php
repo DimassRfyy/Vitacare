cd0a156e9bd2cf768d38f5cd80b203e9c92f5222
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2024-11-30 19:37:10 +0700

    remake view details

--- resources/views/front/details.blade.php
+++ resources/views/front/details.blade.php
@@ -27,7 +27,7 @@
 
     <img src="{{ Storage::url($product->photo) }}" class="h-[220px] w-auto mx-auto relative z-10" alt="">
     <section class="bg-white rounded-t-[60px] pt-[60px] px-6 pb-5 -mt-9 flex flex-col gap-5 max-w-[425px] mx-auto">
-      <div>
+      <div class=" pb-40">
         <div class="flex items-center justify-between">
           <div class="flex flex-col gap-1">
             <p class="font-bold text-[22px]">
@@ -47,85 +47,112 @@
             </p>
           </div>
         </div>
-        <p class="mt-3.5 text-base leading-7">
-          {{ $product->about }}
-        </p>
-      </div>
-
-      <div id="featureSlider" class="-mx-6">
-        <!-- Popular -->
-        <div
-          class="w-[98px] border border-[#f1f1fa] rounded-2xl p-3.5 flex flex-col gap-2.5 items-center justify-center mr-4">
-          <img src="{{asset('assets/svgs/ic-cup-filled.svg')}}" class="size-10" alt="">
-          <p class="text-sm font-semibold">
-            Popular
-          </p>
+        <div id="About" class="flex flex-col gap-1 mt-7">
+          <h2 class="font-semibold">About</h2>
+          <p class="leading-[30px]">{{ $product->about }}</p>
         </div>
-        <!-- Grade A -->
-        <div
-          class="w-[98px] border border-[#f1f1fa] rounded-2xl p-3.5 flex flex-col gap-2.5 items-center justify-center mr-4">
-          <img src="{{asset('assets/svgs/ic-thumb-shape-filled.svg')}}" class="size-10" alt="">
-          <p class="text-sm font-semibold">
-            Grade A
-          </p>
-        </div>
-        <!-- Healthy -->
-        <div
-          class="w-[98px] border border-[#f1f1fa] rounded-2xl p-3.5 flex flex-col gap-2.5 items-center justify-center mr-4">
-          <img src="{{asset('assets/svgs/ic-clipboard-tick-filled.svg')}}" class="size-10" alt="">
-          <p class="text-sm font-semibold">
-            Healthy
-          </p>
-        </div>
-        <!-- Official -->
-        <div
-          class="w-[98px] border border-[#f1f1fa] rounded-2xl p-3.5 flex flex-col gap-2.5 items-center justify-center mr-4">
-          <img src="{{asset('assets/svgs/ic-shiled-tick-filled.svg')}}" class="size-10" alt="">
-          <p class="text-sm font-semibold">
-            Official
-          </p>
-        </div>
-      </div>
-
-      <!-- user Reviews -->
-      <div class="flex flex-col gap-2">
-        <p class="text-base leading-7">
-          My kid was happier whenever he is playing
-          without artificial toys, full energy yeah!
-        </p>
-        <div class="flex items-center justify-between">
-          <div class="flex items-center gap-1.5">
-            <img src="{{asset('assets/images/photo.png')}}" class="size-9" alt="">
-            <p class="text-base font-semibold">
-              Safira
-            </p>
-          </div>
-          <div class="flex">
-            <img src="{{asset('assets/svgs/star.svg')}}" class="size-[18px]" alt="">
-            <img src="{{asset('assets/svgs/star.svg')}}" class="size-[18px]" alt="">
-            <img src="{{asset('assets/svgs/star.svg')}}" class="size-[18px]" alt="">
-            <img src="{{asset('assets/svgs/star.svg')}}" class="size-[18px]" alt="">
-            <img src="{{asset('assets/svgs/star.svg')}}" class="size-[18px]" alt="">
+        <div id="Benefits" class="flex flex-col gap-3 mt-5">
+          <h2 class="font-semibold">Benefits</h2>
+          <div class="grid grid-cols-2 gap-4">
+            <div
+              class="flex p-[18px_14px] outline outline-1 outline-[#EDEEF0] rounded-2xl overflow-hidden justify-start">
+              <div class="flex gap-3 items-center">
+                <div class="size-6 flex shrink-0">
+                  <img src="{{ asset('assets/svgs/note-favorite.svg') }}" alt="icon" class="size-full" />
+                </div>
+                <p class="text-sm leading-[21px] font-semibold text-nowrap">Original 100%</p>
+              </div>
+            </div>
+            <div
+              class="flex p-[18px_14px] outline outline-1 outline-[#EDEEF0] rounded-2xl overflow-hidden justify-start">
+              <div class="flex gap-3 items-center">
+                <div class="size-6 flex shrink-0">
+                  <img src="{{ asset('assets/svgs/dollar-circle.svg') }}" alt="icon" class="size-full" />
+                </div>
+                <p class="text-sm leading-[21px] font-semibold text-nowrap">Cheapest</p>
+              </div>
+            </div>
+            <div
+              class="flex p-[18px_14px] outline outline-1 outline-[#EDEEF0] rounded-2xl overflow-hidden justify-start">
+              <div class="flex gap-3 items-center">
+                <div class="size-6 flex shrink-0">
+                  <img src="{{ asset('assets/svgs/location.svg') }}" alt="icon" class="size-full" />
+                </div>
+                <p class="text-sm leading-[21px] font-semibold text-nowrap">Fast Service</p>
+              </div>
+            </div>
+            <div
+              class="flex p-[18px_14px] outline outline-1 outline-[#EDEEF0] rounded-2xl overflow-hidden justify-start">
+              <div class="flex gap-3 items-center">
+                <div class="size-6 flex shrink-0">
+                  <img src="{{ asset('assets/svgs/shield-tick.svg') }}" alt="icon" class="size-full" />
+                </div>
+                <p class="text-sm leading-[21px] font-semibold text-nowrap">Protected</p>
+              </div>
+            </div>
           </div>
         </div>
+        <a id="promo" href="#" class="px-1 mt-[17px]">
+          <div class="w-full aspect-[353/100] flex shrink-0 overflow-hidden rounded-2xl">
+            <img src="{{asset('assets/images/banner-detail.svg')}}" class="w-full h-full object-cover" alt="promo" />
+          </div>
+        </a>
+        <div id="Testimonials" class="flex flex-col gap-[14px]">
+          <h2 class="font-semibold mt-2">Testimonials</h2>
+          <div class="flex flex-col gap-4">
+            <!-- Testi -->
+            
+            <div class="pb-4 border-b border-[#EDEEF0]">
+              <div class="flex gap-3">
+                <!-- photo -->
+                <div class="size-[50px] flex shrink-0 rounded-full overflow-hidden">
+                  <img src="{{asset('assets/svgs/avatar.svg')}}" alt="icon" class="size-full" />
+                </div>
+                <div class="flex flex-col gap-[6px] w-full">
+                  <div class="flex items-center justify-between">
+                    <p class="text-sm leading-[21px] font-semibold">vanica</p>
+                    <div class="flex gap-[2px]">
+                      <div class="size-5 flex shrink-0">
+                        <img src="{{asset('assets/svgs/star.svg')}}" alt="icon" class="size-full" />
+                      </div>
+                      <div class="size-5 flex shrink-0">
+                        <img src="{{asset('assets/svgs/star.svg')}}" alt="icon" class="size-full" />
+                      </div>
+                      <div class="size-5 flex shrink-0">
+                        <img src="{{asset('assets/svgs/star.svg')}}" alt="icon" class="size-full" />
+                      </div>
+                      <div class="size-5 flex shrink-0">
+                        <img src="{{asset('assets/svgs/star.svg')}}" alt="icon" class="size-full" />
+                      </div>
+                      <div class="size-5 flex shrink-0">
+                        <img src="{{asset('assets/svgs/star.svg')}}" alt="icon" class="size-full" />
+                      </div>
+                    </div>
+                  </div>
+                  <p class="text-xs leading-5 text-[#6E6E70]">lorem ipsum dolor sit amet constrectur</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      
 
       <!-- Price and Add to cart -->
-      <div class="flex items-center justify-between">
-        <div class="flex flex-col gap-0.5">
-          <p class="text-lg min-[350px]:text-2xl font-bold">
-            Rp {{ $product->price }}
-          </p>
-          <p class="text-sm text-grey">
-            /quantity
-          </p>
-        </div>
-        <form action="{{ route('carts.store', $product->id) }}" method="POST">
+      <div class="fixed z-50 bottom-[30px] bg-black rounded-3xl p-5 left-1/2 -translate-x-1/2 w-[calc(100dvw-32px)] max-w-[425px]">
+        <div class="flex items-center justify-between gap-5">
+          <div class="flex flex-col gap-0.5">
+            <p class="text-lg min-[350px]:text-2xl font-bold text-white">
+              Rp.{{ number_format($product->price, 0, ',', '.') }}
+            </p>
+          </div>
+          <form action="{{ route('carts.store', $product->id) }}" method="POST">
             @csrf
             <button type="submit" class="inline-flex w-max text-white font-bold text-base bg-primary rounded-full px-[30px] py-3 justify-center items-center whitespace-nowrap">
               Add to Cart
             </button>
         </form>
+        </div>
       </div>
     </section>
 
